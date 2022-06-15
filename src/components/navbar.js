ed43b9ba176b42746809e0790eb8dbdc5b362353
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2022-06-15 20:10:49 +0530

    adding navbar components

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <nav className="navbar">
+                <p>This is the Navbar</p>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
